a3f5db0b3366e8f0ae4a09f2e0fde1fe8dcc8937
Chris Mems <christopher.mems@.com>
2023-02-26 10:23:53 -0600

    adjust font-size

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = ({ user }) => {
     <div className='w-full h-screen'>
       {/* random quotes */}
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <h1 className='text-xl font-medium'>{user ? '' : 'Welcome to the,'}</h1>
+        <h1 className='sm:text-3xl text-xl font-medium'>{user ? '' : 'Welcome to the,'}</h1>
         <img src={LandingLogo} alt="the fit logo" style={{width:'530px'}}/>
         {/* <div> random quotes go here</div> */}
       </div>
