29cc66a891769795488d339abc0edfe0969c7527
Chris Mems <christopher.mems@.com>
2023-02-28 11:25:34 -0600

    delete line of commented code

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,11 +3,10 @@ import LandingLogo from '../../assets/fittwo.png'
 const Landing = ({ user }) => {
   return (
     <div className='w-full h-screen'>
-      {/* random quotes */}
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <h1 className='sm:text-3xl text-xl font-medium'>{user ? '' : 'Welcome'}</h1>
         <img src={LandingLogo} alt="the fit logo" style={{width:'530px'}}/>
-        {/* <div> random quotes go here</div> */}
+        {/* <div> random motivational quotes go here</div> */}
       </div>
     </div>
   )
